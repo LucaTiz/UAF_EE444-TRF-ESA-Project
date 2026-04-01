2d05e7bacaeb9068fbb66917f42d8110a038f0aa
Adrian Burke <52808661+SushiFanta@users.noreply.github.com>
2026-03-31 16:33:24 -0800

    SPI communication achieved !!
    Rewrite of main code implementing basic SPI communication to TDC board with all other components commented out.

--- mainCode_MSP430F5438A/main.c
+++ mainCode_MSP430F5438A/main.c
@@ -9,12 +9,13 @@ void uartSendChar(char c);
 void uartSendString(const char *str);
 
 void spiWrite(unsigned char data, unsigned char address); 
-int spiRead(unsigned char address);
+void spiRead(unsigned char address);
 //note: SPI sends and receives simultaneously
 
 volatile int tmp = 0;
-volatile int reading = 0;
-volatile int readdata = 0;
+volatile int write = 0x05;
+volatile int read;
+
 
 int main(void)
 {
@@ -22,12 +23,12 @@ int main(void)
 
 
     initClock();	//Setup system timing; do first
-    initUART();		//Initialize UART
-    initSPI();		//Initialize SPI after clock
-    spiWrite(0x05, 0x00);
-    readdata = spiRead(0x00);
+ //   initUART();		//Initialize UART
+    initSPI();		//Initialize SPI after clocks
+    spiWrite(write, 0x00);
+    spiRead(0x00);
     _EINT(); 
-    LPM3;
+    LPM0;
 }
 
 //Clock Setup//
@@ -48,6 +49,71 @@ void initClock(void)
     __delay_cycles(25000);		//Time to allow clock stabilization
 }
 
+// SPI Setup //
+void initSPI(void)
+{
+    P3SEL |= BIT1 | BIT2 | BIT3;	// P3.1 - UCB0SIMO, P3.2 - UCB0SOMI, P3.3 - UCB0CLK, UCB0STE
+    P3DIR |= BIT1 | BIT3;	//STE, MOSI, and CLK pins as output
+    P3DIR &= ~BIT2;			//SOMI as input
+    P1DIR |= BIT3; // CSB control 
+    P1OUT |= BIT3; // set CSB high - not currently in a transaction 
+    P1DIR |= BIT6; // enable pin
+    P1OUT |= BIT6; // set ENABLE high 
+       
+    UCB0CTL1 |= UCSWRST; 		//Hold in reset
+    UCB0CTL0 = UCCKPH | UCMSB | UCMST | UCSYNC | UCMODE_0; //SPI master, MSB first, 3 wire config 
+    UCB0CTL1 = UCSSEL_2;      //SMCLK
+    UCB0BR0 = 8;			//SPI clock divider
+    UCB0BR1 = 0;			//Upper divider byte
+    UCB0CTL1 &= ~UCSWRST;		//Release reset and enable SPI
+    UCB0IE |= UCRXIE;                   //enable interrupt
+}
+
+//write data to a 6-bit address, and recieve the data back as confirmation
+void spiWrite(unsigned char data, unsigned char address)
+{
+    while (!(UCB0IFG & UCTXIFG));	//Wait until TX buffer is empty
+    P1OUT &= ~BIT3;
+    UCB0TXBUF =  (0b01000000 | address);//write command from TDC datasheet - initiates write to address (TDCCONFIG1 at 0x00)
+    while (!(UCB0IFG & UCTXIFG));
+    UCB0TXBUF = data; // writes data (5 to TDCCONFIG1)
+    while (!(UCB0IFG & UCTXIFG));
+    while (UCB0STAT & UCBUSY);
+    P1OUT |= BIT3;
+  //  UCB0TXBUF =  (0x00 | address);//read command to dout on next sclk
+  //  while (!(UCB0IFG & UCTXIFG));
+  //  P1OUT = 1;
+}
+
+//read data from a 6-bit address
+void spiRead(unsigned char address)
+{
+    unsigned char dummy;
+    while (!(UCB0IFG & UCTXIFG));	//Wait until TX buffer is empty
+    P1OUT &= ~BIT3;
+    UCB0TXBUF =  (0b00000000 | address); // Read command (Bit 6 clear)
+    while (UCB0STAT & UCBUSY);
+    dummy = UCB0RXBUF;            // Clear the RX buffer from the first shift
+
+    UCB0TXBUF = 0x00;             // Send dummy to clock in data
+    while (!(UCB0IFG & UCRXIFG));
+    while (UCB0STAT & UCBUSY);
+    read = UCB0RXBUF;          // Capture actual data
+    
+    P1OUT |= BIT3;              // CS High
+}
+/*
+// UCA ISR
+void RX(void) __interrupt [USCI_B0_VECTOR]
+{
+  if(UCB0IFG & UCRXIFG) //Check RX bit of interrupt flag register
+  {
+    tmp = UCB0RXBUF; //record recieved character
+  }
+}
+*/
+
+/*
 //UART Setup//
 void initUART(void)
 {
@@ -69,25 +135,9 @@ void initUART(void)
 
     UCA0CTL1 &= ~UCSWRST; 		//Enable UART
 }
+*/
 
-// SPI Setup //
-void initSPI(void)
-{
-    P3SEL |= BIT0 | BIT1 | BIT2 | BIT3;	//UCB0SIMO, UCB0SOMI, UCB0CLK, UCB0STE
-    P3DIR |= BIT0 | BIT1 | BIT3;	//STE, MOSI, and CLK pins as output
-    P1DIR |= BIT3; // CSB control 
-    P1OUT |= BIT3; // set CSB high - not currently in a transaction 
-    P1DIR |= BIT6; // enable pin
-    P1OUT |= BIT6; // set ENABLE high 
-    P3DIR &= ~BIT2;			//SOMI as input   
-    UCB0CTL1 |= UCSWRST; 		//Hold in reset
-    UCB0CTL0 = UCCKPH | UCMSB | UCMST | UCSYNC | UCMODE_0; //SPI master, MSB first, 3 wire config 
-    UCB0CTL1 = UCSWRST | UCSSEL_2;      //SMCLK
-    UCB0BR0 = 8;			//SPI clock divider
-    UCB0BR1 = 0;			//Upper divider byte
-    UCB0CTL1 &= ~UCSWRST;		//Release reset and enable SPI
-    UCB0IE |= UCRXIE;                   //enable interrupt
-}
+/*
 //UART transmit functions
 void uartSendChar(char c)
 {
@@ -101,46 +151,4 @@ void uartSendString(const char *str)
          uartSendChar(*str++);
     }
 }    
-
-//write data to a 6-bit address, and recieve the data back as confirmation
-void spiWrite(unsigned char data, unsigned char address)
-{
-    while (!(UCB0IFG & UCTXIFG));	//Wait until TX buffer is empty
-    P1OUT &= ~BIT3;
-    UCB0TXBUF =  (0b01000000 | address);//write command form TDC datasheet - initiates write to address (TDCCONFIG1 at 0x00)
-    while (!(UCB0IFG & UCTXIFG));
-    UCB0TXBUF = data; // writes data (5 to TDCCONFIG1)
-    while (!(UCB0IFG & UCTXIFG));
-    P1OUT |= BIT3;
-  //  UCB0TXBUF =  (0x00 | address);//read command to dout on next sclk
-  //  while (!(UCB0IFG & UCTXIFG));
-  //  P1OUT = 1;
-}
-
-//read data from a 6-bit address
-int spiRead(unsigned char address)
-{
-    
-    while (!(UCB0IFG & UCTXIFG));	//Wait until TX buffer is empty
-    P1OUT &= ~BIT3;
-    UCB0TXBUF =  (0x00 | address);//read command
-    while (!(UCB0IFG & UCTXIFG));
-    UCB0TXBUF = 0; // writes dummy data 
-    while (!(UCB0IFG & UCTXIFG));
-    while (!(UCB0IFG & UCRXIFG));
-
-    reading =  UCB0RXBUF;
-    
-    
-    P1OUT |= BIT3;
-    return reading;
-}
-
-// UCA ISR
-void RX(void) __interrupt [USCI_B0_VECTOR]
-{
-  if(UCB0IFG & UCRXIFG) //Check RX bit of interrupt flag register
-  {
-    tmp = UCB0RXBUF; //record recieved character
-  }
-}
+*/
