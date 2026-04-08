001ea2d4a8ae2bc4ab400839eb6b6c2f5d6834f7
Asiago-cheese <wruiz@alaska.edu>
2026-04-07 22:48:38 -0800

    updated uart baud settings

--- mainCode_MSP430F5438A/uart.c
+++ mainCode_MSP430F5438A/uart.c
@@ -8,18 +8,23 @@ void initUART(void)
 
     UCA0CTL1 |= UCSWRST;		//Hold USCI in reset while configuring
     UCA0CTL1 |= UCSSEL_2;		//SMCLK as source
-    UCOS16 = 1;
+    //UCOS16 = 1; //Wrong implimentation, bit part of UCA0MCTL
     //table 
 
-    //Divider for Clock:
-    //Divider = Clk / baud rate = 8e6 / 9600 = 833.333
+    //UART Baud setup depends on mode:
+    //If UC0S16 =0 then N = CLK / Baud : (non-oversampling)
+    //N = Clk / baud rate = 8e6 / 9600 = 833.333
     //BR0/BR1 = 833 
-    //note: MSP430 can not store 833 in single register, max value per register is 255
+    //If UC0S16 = 1 then N = CLK / (16 * Baud) : (Oversample)
+    //N = CLK / Baud = 8e6 / (16*115200) = 4.34...
+    // Fractional portion: 0.3403; modulation value: 0.3403*16 = 5.44
+    //BR0/BR1 = 4/0
+    // note: MSP430 can not store 833 in single register, max value per register is 255
     //Values are stored in two registers instead
 
-    UCA0BR0 = 833 & 0xFF;		//lower 8 bits (65)
-    UCA0BR1 = (833 >> 8);		//upper 8 bits (3 * 2^8)
-    UCA0MCTL = UCBRS_6;			//Approx modulation
+    UCA0BR0 = 4;		    //lower divider byte
+    UCA0BR1 = 0;		    //upper divider byte
+    UCA0MCTL = UCBRF_5 | UCBRS_0 | UCOS16;	// 1st stage modulation(fractional baud correction) + 2nd stage modulation + oversampling
 
     UCA0CTL1 &= ~UCSWRST; 		//Enable UART
 }
