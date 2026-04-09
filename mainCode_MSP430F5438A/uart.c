adf542e0bd34fdeaf280d67d57f5abe69ce7b4a9
Adrian Burke <52808661+SushiFanta@users.noreply.github.com>
2026-04-09 14:05:18 -0800

    wenceslao fix uart.c

--- mainCode_MSP430F5438A/uart.c
+++ mainCode_MSP430F5438A/uart.c
@@ -1,19 +1,18 @@
 #include <msp430.h>
 
-//UART Setup//
+//UART Setup// 57600 baud, oversampling, 8 data, 1 stop, even parity
 void initUART(void)
 {
     //Select UART pins (double check)
-    P3SEL |= BIT4 | BIT5;		// P3.4 = TX and P3.5 = RX
-    UCA0CTL0 = UCPEN + UCPAR;		//even parity, 8 data bits, 1 stop bit
-    UCA0CTL1 |= UCSWRST;		//Hold USCI in reset while configuring
-    UCA0CTL1 |= UCSSEL_2;		//SMCLK as source
+    P5SEL |= BIT6 | BIT7;		//P5.6 = UCA1TXD and P5.7 = UCA1RXD; for UCA1
+    UCA1CTL1 |= UCSWRST;		//Hold USCI in reset while configuring
+    UCA1CTL1 |= UCSSEL_2;		//SMCLK as source
+    UCA1CTL0 = UCPEN + UCPAR;	//even parity, 8 data bits, 1 stop bit
     //Divider for Clock:
-    //Divider = Clk / baud rate = 8 MHz / 115200
-    UCA0BR0 = 69	;		//lower 8 bits
+    //oversampling on, reccommended values from table 36-5
+    UCA0BR0 = 8;		//lower 8 bits
     UCA0BR1 = 0;			//upper 8 bits
-    UCA0MCTL = UCBRS_4;			//oversampling off, reccommended values from table 36-4
-
+    UCA0MCTL = UCBRS_0 | UCBRF_11 | UCOS16;
     UCA0CTL1 &= ~UCSWRST; 		//Enable UART
 }
 
